83375fbf652af17402802afaf80bfaf95ea3da71
tuzmusic <tuzmusic@gmail.com>
2019-05-24 17:54:10 -0400

    configure headless protractor

--- e2e-tests/protractor.conf.js
+++ e2e-tests/protractor.conf.js
@@ -5,7 +5,11 @@ exports.config = {
   specs: ["*.js"],
 
   capabilities: {
-    browserName: "chrome"
+    directConnect: true,
+    browserName: "chrome",
+    chromeOptions: {
+      args: ["--headless", "--disable-gpu", "--window-size=800x600"]
+    }
   },
 
   baseUrl: "http://0.0.0.0:8000",
