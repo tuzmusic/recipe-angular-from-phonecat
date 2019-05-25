ab54184131c5e62a7ae8b36217abdfe2d3f22c36
tuzmusic <tuzmusic@gmail.com>
2019-05-25 15:06:12 -0400

    render slide in recipe detail

--- app/recipe-detail/recipe-detail.component.js
+++ app/recipe-detail/recipe-detail.component.js
@@ -1,10 +1,14 @@
-function RecipeDetailController($http, $routeParams) {
-  $http.get("api-data/recipes/" + $routeParams.recipeId + ".json").then(res => {
-    this.recipe = res.data;
-  });
-}
-
 angular.module("recipeDetail").component("recipeDetail", {
   templateUrl: "recipe-detail/recipe-detail.template.html",
-  controller: ["$http", "$routeParams", RecipeDetailController]
+  controller: [
+    "$http",
+    "$routeParams",
+    function RecipeDetailController($http, $routeParams) {
+      $http
+        .get("api-data/recipes/" + $routeParams.recipeId + ".json")
+        .then(res => {
+          this.recipe = res.data;
+        });
+    }
+  ]
 });
--- app/step-slide/step-slide.component.js
+++ app/step-slide/step-slide.component.js
@@ -1,6 +1,5 @@
 angular.module("recipeDetail").component("stepSlide", {
   template: "<p class='step-text'>Mix ingredients</p>",
-  // templateUrl: "step-slide/step-slide.template.html",
   controller: function StepSlideController() {
     this.step = {
       text: "Add 1 tbsp of sugar"
