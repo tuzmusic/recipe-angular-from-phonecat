7a7132f112150d2632fdf26925b87c64df052ba9
tuzmusic <tuzmusic@gmail.com>
2019-05-25 17:43:49 -0400

    remove extraneous stuff from step-slide-component

--- app/modules/recipe-detail/step-slide/step-slide.component.js
+++ app/modules/recipe-detail/step-slide/step-slide.component.js
@@ -1,11 +1,5 @@
 angular.module("recipeDetail").component("stepSlide", {
   templateUrl: "modules/recipe-detail/step-slide/step-slide.template.html",
-  transclude: true,
-  // require: { recipeCtrl: "^recipeDetail" },
   bindings: { step: "<" },
-  controller: function StepSlideController() {
-    this.step = {
-      text: "Add 1 tbsp of sugar"
-    };
-  }
+  controller: function StepSlideController() {}
 });
